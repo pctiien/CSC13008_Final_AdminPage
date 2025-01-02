a2f3aafad4d4e3dbffd9022504d07bc582719580
pctien <pctien1401@gmail.com>
2025-01-02 22:30:12 +0700

    report: format to vi-vn current and datetime

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,15 +1,15 @@
 export const formatCurrency = (amount) => {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('vi-VN', {
       style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
+      currency: 'VND',
+      minimumFractionDigits: 0,
     }).format(amount);
   };
   
   export const formatDate = (date) => {
-    return new Intl.DateTimeFormat('en-US', {
+    return new Intl.DateTimeFormat('vi-VN', {
       year: 'numeric',
-      month: 'short',
+      month: 'long',
       day: 'numeric',
     }).format(date);
   };
